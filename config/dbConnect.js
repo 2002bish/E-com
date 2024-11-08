51e5f971f25ea84400fb20040115aa57ddcfe22d
2002bish <bniroula707@gmail.com>
2024-11-08 14:04:20 +0545

    fix error

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
     try {
-        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`, { // Ensure DB_NAME is in your .env file
-      
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+        );
         console.log(`\nMongoDB connected! DB HOST: ${conn.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection FAILED", error);
