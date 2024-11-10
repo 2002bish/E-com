aa3c8c9ec28f0dc7fcfda659a410343e601c92be
2002bish <bniroula707@gmail.com>
2024-11-10 16:24:34 +0545

    added route file

---
+++ routes/deliveryRoute.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const router = 'express.Router';
+const deliverycontroller =require ("../controllers/deliverycontroller");
+
+//Assign Delivery Task
+route.put('/delivery/assign', deliverycontroller.assignDelivery);
+
+//For real time location tracking
+router.put('/delivery/location', deliverycontroller.updateDeliveryLocation);
+
+
+
+//If Delivery is completed
+router.put('/delivery/complete', deliverycontroller.completeDelivery);
+
+//History of the Delivery
+router.get('/delivery/history/:deliveryPersonId', deliverycontroller.getDeliveryHistory)
+
+module.exports =router;
