38c6ea2ad5be57d6988d0cabc7a45c33a6b3b913
2002bish <bniroula707@gmail.com>
2024-11-19 14:55:13 +0545

    fix the issue

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
-const productRoute =require("./routes/productRoute");
-const orderRoute =require("./routes/orderRoute");
-const deliveryRoute= require("./routes/deliveryRoute");
-const discountRoute = require("./routes/discountRoute");
-const promotionRoute = require ("./routes/promotionRoute");
-const ticketRoute = require("./routes/ticketRoute");
-const refundRoute = require("./routes/refundRoute");
-const reportRoute = require("./routes/reportRoute");
-const notificationRoute = require("./routes/notificationRoute");
-const settingRoute = require('./routes/settingRoute');
+const productRoute =require("../routes/productRoute");
+const orderRoute =require("../routes/orderRoute");
+const deliveryRoute= require("../routes/deliveryRoute");
+const discountRoute = require("../routes/discountRoute");
+const promotionRoute = require ("../routes/promotionRoute");
+const ticketRoute = require("../routes/ticketRoute");
+const refundRoute = require("../routes/refundRoute");
+const reportRoute = require("../routes/reportRoute");
+const notificationRoute = require("../routes/notificationRoute");
+const settingRoute = require('../routes/settingRoute');
 
 // Database Connection
 dbConnect();
